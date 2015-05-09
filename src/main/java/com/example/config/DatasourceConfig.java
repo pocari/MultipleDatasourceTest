73ebf61c5295902695e14297b236d3399b973b98
pocari <caffelattenonsugar@gmail.com>
2015-05-09 18:23:29 +0900

    不要な処理、コメントを削除

--- src/main/java/com/example/config/DatasourceConfig.java
+++ src/main/java/com/example/config/DatasourceConfig.java
@@ -16,33 +16,23 @@ public class DatasourceConfig {
     @Bean(name="ds1")
     @Primary
     public DataSource ds1() {
-        System.out.println("yn dataSourceOne created");
         DataSource dataSource = createJndiDataSourceBy("java:/jdbc/XAds1");
-//        DataSource dataSource = createJndiDataSourceBy("java:jboss/jdbc/HogeSystemXA");
-        System.out.println("dataSourceOne:" + dataSource);
         return dataSource;
     }
 
     @Bean(name="ds2")
     public DataSource ds2() {
-        System.out.println("yn dataSourceTwo created");
         DataSource dataSource = createJndiDataSourceBy("java:/jdbc/XAds2");
-//        DataSource dataSource = createJndiDataSourceBy("java:jboss/jdbc/FooSystemXA");
-        System.out.println("dataSourceTwo:" + dataSource);
         return dataSource;
     }
 
     @Bean
     public JdbcTemplate jdbcTemplate(@Qualifier("ds1") DataSource dataSource) {
-        System.out.println("yn jdbcTemplate created");
-        System.out.println("dataSource:" + dataSource);
         return new JdbcTemplate(dataSource);
     }
 
     @Bean
     public JdbcTemplate jdbcTemplate2(@Qualifier("ds2") DataSource dataSource) {
-        System.out.println("yn jdbcTemplate2 created");
-        System.out.println("dataSource:" + dataSource);
         return new JdbcTemplate(dataSource);
     }
 
